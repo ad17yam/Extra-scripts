07bcc5d3e793e7e4cc30ca03942a36b361e52924
admurray <ad17yam@gmail.com>
2016-07-12 00:54:29 -0400

    Added print statements

--- app_tests/fuzzy_search.py
+++ app_tests/fuzzy_search.py
@@ -9,23 +9,32 @@ from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
 print 'Simple Ratio'
-fuzz.ratio("this is a test", "this is a test!")
+print 'this is a test, this is a test!'
+print fuzz.ratio("this is a test", "this is a test!")
 
 print 'Partial Ratio'
-fuzz.partial_ratio("this is a test", "this is a test!")
+print 'this is a test, this is a test!'
+print fuzz.partial_ratio("this is a test", "this is a test!")
 
 print 'Token Sort Ratio'
-fuzz.ratio("fuzzy wuzzy was a bear", "wuzzy fuzzy was a bear")
+print "fuzzy wuzzy was a bear, wuzzy fuzzy was a bear"
+print fuzz.ratio("fuzzy wuzzy was a bear", "wuzzy fuzzy was a bear")
 
-fuzz.token_sort_ratio("fuzzy wuzzy was a bear", "wuzzy fuzzy was a bear")
+print "fuzzy wuzzy was a bear, wuzzy fuzzy was a bear "
+print fuzz.token_sort_ratio("fuzzy wuzzy was a bear", "wuzzy fuzzy was a bear")
 
 print 'Token Set Ratio'
-fuzz.token_sort_ratio("fuzzy was a bear", "fuzzy fuzzy was a bear")
-fuzz.token_set_ratio("fuzzy was a bear", "fuzzy fuzzy was a bear")
+print "fuzzy was a bear fuzzy fuzzy was  a bear"
+print fuzz.token_sort_ratio("fuzzy was a bear", "fuzzy fuzzy was a bear")
+
+print "fuzzy was a bear fuzzy fuzzy was a bear"
+print fuzz.token_set_ratio("fuzzy was a bear", "fuzzy fuzzy was a bear")
 
 print 'Process'
 choices = ["Atlanta Falcons", "New York Jets", "New York Giants", "Dallas Cowboys"]
-process.extract("new york jets", choices, limit=2)
+print 'new york jets'
+print process.extract("new york jets", choices, limit=2)
 
-process.extractOne("cowboys", choices)
+print 'cowboys'
+print process.extractOne("cowboys", choices)
 
